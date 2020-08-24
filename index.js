1f9d69de330aaf9929eac923b89d9bd2d6584346
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2020-08-24 23:43:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ function findAllPermutations(str) {
       permutations.push(prefix);
     } else {
       for(let i = 0; i < suffix.length; i++){
-        permutate(prefix + suffix[i], suffix.slice(0, i) + suffix.slice(i + 1, suffix.length))
+        permutate(prefix + suffix[i], suffix.slice(0, i) + suffix.slice(i + 1, suffix.length));
       }
     }
   }
+  permutate("", str);
+
+  return permutations;
 }
